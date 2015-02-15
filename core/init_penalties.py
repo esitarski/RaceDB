246f4295e58b0af2633a005213b6bd6ded382388
Edward Sitarski <edward.sitarski@gmail.com>
2015-02-15 17:08:19 -0500

    Commented out penalties (for now).

--- core/init_penalties.py
+++ core/init_penalties.py
@@ -1,115 +1,115 @@
 # coding=utf-8
 
-track_penalties = \
-u'''For not holding his/her line during the sprint
-Pour avoir quitté sa ligne pendant le sprint final
-Por desviarse de su linea durante el sprint final
+# track_penalties = \
+# u'''For not holding his/her line during the sprint
+# Pour avoir quitté sa ligne pendant le sprint final
+# Por desviarse de su linea durante el sprint final
 
-For riding on the blue and during the sprint
-Pour avoir roulé sur la zone bleu lors du sprint
-Por pasar por la zona azul durante el sprint
+# For riding on the blue and during the sprint
+# Pour avoir roulé sur la zone bleu lors du sprint
+# Por pasar por la zona azul durante el sprint
 
-For not having hel his/her line during the last 200 meters of the race
-Pour ne pas avoir tenu sa ligne dans les 200 derniears metres de l’épreuve
-Por no mantener su linea en los últimos 200 metros de la prueba
+# For not having hel his/her line during the last 200 meters of the race
+# Pour ne pas avoir tenu sa ligne dans les 200 derniears metres de l’épreuve
+# Por no mantener su linea en los últimos 200 metros de la prueba
 
-For irregular movement to prevent his opponent from passing
-Pour manoeuvre illicit, faite poir empêcher son adveraire de passer
-Por maniobra irregular para impeder el paso de su oponente
+# For irregular movement to prevent his opponent from passing
+# Pour manoeuvre illicit, faite poir empêcher son adveraire de passer
+# Por maniobra irregular para impeder el paso de su oponente
 
-For dangerous riding in the final bend
-Pour manoeuvre dangereuse dans le dernier virage
-Por maniobra peligrosa en la ultima curva
+# For dangerous riding in the final bend
+# Pour manoeuvre dangereuse dans le dernier virage
+# Por maniobra peligrosa en la ultima curva
 
-For dangerous riding in the race
-Pour manoeuvre dangereuse au cours de l’épreuve
-Por movimientos peligrosos durante la prueba
+# For dangerous riding in the race
+# Pour manoeuvre dangereuse au cours de l’épreuve
+# Por movimientos peligrosos durante la prueba
 
-For entering the sprinter’s lane when the opponent was already there
-Pour être entré dan le couloirs des sprinters alors que son adveraire s’y trouvait déjà
-Por entrar en el passillio de sprinters cuanda estaba ocupado por su adversario
+# For entering the sprinter’s lane when the opponent was already there
+# Pour être entré dan le couloirs des sprinters alors que son adveraire s’y trouvait déjà
+# Por entrar en el passillio de sprinters cuanda estaba ocupado por su adversario
 
-For moving down towards the inside of the track when a rival was already there
-Pour s’être rabattu vers l’interieur alors que son concurrent s’y trouvait déjà
-Por movimeinto brusco hacia el interior donde se encontraba su openente
+# For moving down towards the inside of the track when a rival was already there
+# Pour s’être rabattu vers l’interieur alors que son concurrent s’y trouvait déjà
+# Por movimeinto brusco hacia el interior donde se encontraba su openente
 
-For moving down towards the inside of the track and forcing the other competitor off the track
-Pour s’être rabattu á la corde obligeant ainsi son adversaire à sortir de la piste
-Por desplazamiento hacia el interior forzando a su adversario a salir de la pista
+# For moving down towards the inside of the track and forcing the other competitor off the track
+# Pour s’être rabattu á la corde obligeant ainsi son adversaire à sortir de la piste
+# Por desplazamiento hacia el interior forzando a su adversario a salir de la pista
 
-For crowding his/her opponent with the intention of causing him to slow down
-Pour avoir tassé l’adversaire dans le but de le faire ralentir
-Por presionar cerrando al adversario para obligarie a disminuir su marcha
+# For crowding his/her opponent with the intention of causing him to slow down
+# Pour avoir tassé l’adversaire dans le but de le faire ralentir
+# Por presionar cerrando al adversario para obligarie a disminuir su marcha
 
-For moving outward with the intention of forcing the opponentto go up
-Pour avoir dévié de sa trajectoure et avoir conduit son adversaire vers l’extérieur de la piste
-Por presionar cerrando al adversario para obligarie a subir hacia el exterior
+# For moving outward with the intention of forcing the opponentto go up
+# Pour avoir dévié de sa trajectoure et avoir conduit son adversaire vers l’extérieur de la piste
+# Por presionar cerrando al adversario para obligarie a subir hacia el exterior
 
-For going down to quickly after overtaking his/her opponent
-Pour s’être rabattu trop rapidement après avoir dépassé son adversaire
-Por descender demasiado rápido y cerrar a su adversario después de adelantarie
+# For going down to quickly after overtaking his/her opponent
+# Pour s’être rabattu trop rapidement après avoir dépassé son adversaire
+# Por descender demasiado rápido y cerrar a su adversario después de adelantarie
 
-For deliberate and flagrant fault against {license_holder_victim}
-Pour faute flagrante et volontaire contre {license_holder_victim}
-Por accion flagrante y voluntaria en ctra de {license_holder_victim}
+# For deliberate and flagrant fault against {license_holder_victim}
+# Pour faute flagrante et volontaire contre {license_holder_victim}
+# Por accion flagrante y voluntaria en ctra de {license_holder_victim}
 
-For causing the crash of his/her opponent
-Pour avoir provoqué la chue de son adversaire
-Por provocar la caida de su adversario
+# For causing the crash of his/her opponent
+# Pour avoir provoqué la chue de son adversaire
+# Por provocar la caida de su adversario
 
-For having blocked an opponent
-Pour avoir gêné son adversaire
-Por acciones de bloqueo sobre su oponente
+# For having blocked an opponent
+# Pour avoir gêné son adversaire
+# Por acciones de bloqueo sobre su oponente
 
-For being late at the start line
-Pour retard au depart
-Por presentarse con retraso a la linea de salida
+# For being late at the start line
+# Pour retard au depart
+# Por presentarse con retraso a la linea de salida
 
-For wearing only one number
-Pour avoir porté un seul dossard
-Por lievar un solo dorsal
+# For wearing only one number
+# Pour avoir porté un seul dossard
+# Por lievar un solo dorsal
 
-For incorrect gestures
-Pour geste incorrect
-Por realizar gestos incorrectos
+# For incorrect gestures
+# Pour geste incorrect
+# Por realizar gestos incorrectos
 
-For incorrect behavior
-Pour comportement incorrect
-Por comportamento incorrect
+# For incorrect behavior
+# Pour comportement incorrect
+# Por comportamento incorrect
 
-For pushing his/her rival
-Pour avoir poussé son adversaire
-Por empujon sobre su oponente
+# For pushing his/her rival
+# Pour avoir poussé son adversaire
+# Por empujon sobre su oponente
 
-For carrying advertisement on the back of the racing jersey
-Pour port de publicité sur le dos du maillot
-Por llevar publicidad in la parte posterior del maillot
+# For carrying advertisement on the back of the racing jersey
+# Pour port de publicité sur le dos du maillot
+# Por llevar publicidad in la parte posterior del maillot
 
-For incorrect behavior or disrespect towards an official
-Pour comportement incorrect envars un Commissaire
-Por comportamiento incorrect hacia un commissario
+# For incorrect behavior or disrespect towards an official
+# Pour comportement incorrect envars un Commissaire
+# Por comportamiento incorrect hacia un commissario
 
-For folding or mutilating the race umber
-Pour avoir plié ou modifié son dossard
-Por llevar el dorsal doblado o modificado
+# For folding or mutilating the race umber
+# Pour avoir plié ou modifié son dossard
+# Por llevar el dorsal doblado o modificado
 
-For improper advertizing on national jersey or shorts
-Pour publicité non conforme sur le maillot/cuissard/équipement
-Por publicidad no autorizada en la vestimenta
+# For improper advertizing on national jersey or shorts
+# Pour publicité non conforme sur le maillot/cuissard/équipement
+# Por publicidad no autorizada en la vestimenta
 
-Qualified to start {event} but did not start without justification
-Qualifié pour les {event} et absent au depart sans justification
-Classificado para {event} y ausente sin justification
+# Qualified to start {event} but did not start without justification
+# Qualifié pour les {event} et absent au depart sans justification
+# Classificado para {event} y ausente sin justification
 
-For protest with hands off handlebar
-Pour avoir levé la main en guise de protestation
-Por realizar gestor de protesta levantando las manos del manillar
+# For protest with hands off handlebar
+# Pour avoir levé la main en guise de protestation
+# Por realizar gestor de protesta levantando las manos del manillar
 
-For using two persons to give information to the team/rider
-Pour avoir utilise deux personnes pour informer son équipe/coureur
-Por utilizar dos personas para informar a su equipo
+# For using two persons to give information to the team/rider
+# Pour avoir utilise deux personnes pour informer son équipe/coureur
+# Por utilizar dos personas para informar a su equipo
 
-For not being ready with extra wheels or other equipement at the start
-Pour insuffisance de matériel de recharge au depart
-Por no estar preparado con material de recambio
-'''
+# For not being ready with extra wheels or other equipement at the start
+# Pour insuffisance de matériel de recharge au depart
+# Por no estar preparado con material de recambio
+# '''
