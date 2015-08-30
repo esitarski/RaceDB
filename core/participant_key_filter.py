ce689de8cbb2c564e99144ff5643205f976b3145
Edward Sitarski <edward.sitarski@gmail.com>
2015-08-30 17:30:09 -0400

    Added lstrip to scan search.

--- core/participant_key_filter.py
+++ core/participant_key_filter.py
@@ -10,7 +10,7 @@ def participant_key_filter( competition, key, auto_add_participant = True ):
 	# Convert the key to upper.
 	# The license_code, tag and tag2 are all stored in upper-case, so it is safe to do
 	# exact matches.
-	key = key.strip().upper()
+	key = key.strip().upper().lstrip( '0' )
 	
 	if not key:
 		return None, []
