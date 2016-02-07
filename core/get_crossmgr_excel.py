c5cc603acf6094c0180c8acaa901c2ef8a88a7c3
Edward Sitarski <edward.sitarski@gmail.com>
2016-02-07 18:50:53 -0500

    Fixed bug in excel extract with win and out.

--- core/get_crossmgr_excel.py
+++ core/get_crossmgr_excel.py
@@ -211,7 +211,7 @@ def add_properties_page( wb, title_format, event, raceNumber ):
 		competition.ga_tracking_id,
 		event.road_race_finish_times,
 		event.dnsNoData,
-		getattr('win_and_out', False),
+		getattr(event, 'win_and_out', False),
 	]
 	row = write_row_data( ws, row, row_data )
 
