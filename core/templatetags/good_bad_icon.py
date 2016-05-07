5d49079d4c8243758a9fb87da1c83929089e0837
Edward Sitarski <edward.sitarski@gmail.com>
2016-05-07 08:19:48 -0400

    Added template tags to improve icon display in list views.

---
+++ core/templatetags/good_bad_icon.py
@@ -0,0 +1,12 @@
+from django import template
+from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def good_bad_icon( value ):
+	if value:
+		return mark_safe('<span class="is-good"/>')
+	else:
+		return mark_safe('<span class="is-bad"/>')
