caf4716e544c81d9d05e0fbf8c74921d4c2fbbef
Edward Sitarski <edward.sitarski@gmail.com>
2016-07-28 08:07:13 -0400

    Updated official release.

--- pypi.py
+++ pypi.py
@@ -8,7 +8,7 @@ from dependencies import pyllrp
 from helptxt.version import version
 
 # Delete any existing unofficial releases.
-officialRelease = '1.9.0'
+officialRelease = '1.10.0'
 	
 def pypi():
 	CompileHelp( 'helptxt' )
