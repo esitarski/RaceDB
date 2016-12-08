001154e4c9a3d0acdc45486eb5bbc0c331542eae
Edward Sitarski <edward.sitarski@gmail.com>
2016-12-07 21:51:27 -0500

    improved 2-per-page bib format

--- core/views.py
+++ core/views.py
@@ -832,7 +832,7 @@ def GetCompetitionForm( competition_cur = None ):
 				Row(
 					Col('ftp_upload_during_race', 4),
 				),
-				Row( HTML('<hr/>') ),
+				Row( HTML('<hr/><strong>Number Print Options:</strong>') ),
 				Row( Col(Field('bibs_label_print'),3), Col(Field('bibs_laser_print'),3), Col(Field('shoulders_label_print'),3), Col(Field('frame_label_print'),3) ),
 				Row( HTML('<hr/>') ),
 				Row(
