568ac2c5880f0172c100510a613e9cb82ca567cb
Edward Sitarski <edward.sitarski@gmail.com>
2017-07-31 16:38:22 -0400

    Fixed problem with automatically creating the laucn link.

--- CreateShortcut.py
+++ CreateShortcut.py
@@ -4,14 +4,14 @@ import subprocess
 
 vbTemplate = '''
 Set oWS = WScript.CreateObject("WScript.Shell")
-Set oLink = oWS.CreateShortcut("{link}")
-
+strDesktop = oWS.SpecialFolders("Desktop")
+Set oLink = oWS.CreateShortcut(strDesktop + "\{name}.lnk")
 oLink.TargetPath = "{targetPath}"
+oLink.WorkingDirectory = "{workingDirectory}"
 oLink.Arguments = "{arguments}"
 oLink.Description = "{description}"
 oLink.IconLocation =  "{icon}"
-oLink.WorkingDirectory = "{workingDirectory}"
-oLink.WindowStyle = 1
+oLink.WindowStyle = 4
 oLink.Save
 '''
 
@@ -23,27 +23,30 @@ def CreateShortcut( cmd="launch" ):
 	current_folder = os.path.dirname(os.path.realpath(__file__))
 	
 	vbStr = vbTemplate.format(
-		link			=os.path.join( current_folder, 'RaceDB Launch.LNK' ),
+		name			='RaceDB Launch',
 		targetPath		=sys.executable,
 		arguments		=' '.join( ("manage.py", cmd) ),
-		description		="RaceDB",
+		description		="RaceDB Launch",
 		icon			='{},{}'.format(os.path.join(current_folder, 'core', 'static', 'images', 'RaceDB.ico'), 0),
 		workingDirectory=current_folder,
 	)
 	
-	print vbStr
+	#fd, fname = tempfile.mkstemp( suffix='.vbs' )
+	#f = os.fdopen( fd, 'wb' )
 	
-	fd, fname = tempfile.mkstemp( suffix='.vbs' )
-	f = os.fdopen( fd, 'wb' )
+	fname = os.path.join( current_folder, 'create_shortcut.vbs' )
+	f = open( fname, 'w' )
+	
 	f.write( vbStr )
 	f.close()
 	
 	try:
-		subprocess.call( ['CScript.exe', fname], shell=True )
+		subprocess.call( ['cscript.exe', '//Nologo', fname], shell=True )
 	except Exception as e:
 		print 'Failed: ', e
 	finally:
-		os.remove( fname )
+		#os.remove( fname )
+		pass
 
 if __name__ == '__main__':
 	CreateShortcut()
